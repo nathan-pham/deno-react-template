7da89884906a65fa8d83bdf5bc2f7b3ccf7dbfbb
Nathan Pham <nathanpham.me@gmail.com>
2022-08-26 15:20:01 -0700

    remove three as a dependency (was for a diff project not a template)

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { default as React } from "https://esm.sh/react@17.0.1?target=deno";
 export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.1?target=deno";
-export * as THREE from "https://cdn.skypack.dev/three@0.143.0?dts";
